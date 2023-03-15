05f324b18271aced8dedc510c656c9fd8d7f9b59
Dew-OF-Aurora <1962662993@qq.com>
2023-03-15 22:14:17 +0800

    3.15

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>index</div>
+  <iframe
+    width="100%"
+    height="700px"
+    src="https://cybermap.kaspersky.com/cn/widget/dynamic/dark"
+    frameborder="5"
+  ></iframe>
 </template>
 
 <script>
